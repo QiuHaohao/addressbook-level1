535ade112974b43fc3f9430c00a6e7e406db8a06
Qiu Haohao <qiuh0008@e.ntu.edu.sg>
2018-02-01 14:03:06 +0800

    Modified the logic in extractNameFromPersonString

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -974,8 +974,9 @@ public class AddressBook {
     private static String extractNameFromPersonString(String encoded) {
         final int indexOfPhonePrefix = encoded.indexOf(PERSON_DATA_PREFIX_PHONE);
         final int indexOfEmailPrefix = encoded.indexOf(PERSON_DATA_PREFIX_EMAIL);
+        final int indexOFGitHubPrefix = encoded.indexOf(PERSON_DATA_PREFIX_EMAIL);
         // name is leading substring up to first data prefix symbol
-        int indexOfFirstPrefix = Math.min(indexOfEmailPrefix, indexOfPhonePrefix);
+        int indexOfFirstPrefix = Math.min(indexOfEmailPrefix, indexOfPhonePrefix, indexOFGitHubPrefix);
         return encoded.substring(0, indexOfFirstPrefix).trim();
     }
 
